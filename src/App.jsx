cf973b168775dc3ac924573313a9552672542633
Laura Nguyen <hello@lauranguyen.me>
2024-07-08 12:49:34 -0400

    Added structure for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import StretchesPage from "./pages/StretchesPage/StretchesPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
+import "./App.scss";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <StretchesPage />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/stretches" element={<StretchesPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,93 @@
+import "./HomePage.scss";
+import heroImage from "./../../assets/images/warrior ii.jpeg";
+
+const HomePage = () => {
+  return (
+    <>
+      <main className="main">
+        <div className="main__copy">
+          <h1>Get Fit Anywhere, Anytime</h1>
+          <p>
+            Kick-start your fitness journey with Flexifit AI, your personal AI
+            trainer that brings expert-guided workouts right to your home.
+            Whether you're a busy professional or just starting your fitness
+            journey, Flexifit AI offers tailored workout programs, real-time
+            feedback, and progress tracking to help you achieve your fitness
+            goals.
+          </p>
+          <button>Start Your Free Trial</button>
+          <a href="#about">Learn More About Us</a>
+        </div>
+        <img className="main__img" src={heroImage} alt="" />
+      </main>
+
+      <section className="about">
+        <h2>About Flexifit AI</h2>
+        <p>
+          At Flexifit AI, we believe that everyone deserves the opportunity to
+          be fit and healthy, regardless of their schedule or location. Our
+          intelligent personal trainer uses advanced AI and machine learning
+          technologies to provide personalized workout programs, real-time
+          feedback, and detailed progress tracking. Join us and see how easy it
+          is to integrate fitness into your daily routine.
+        </p>
+        <h3>Get Started in 3 Simple Steps</h3>
+        <ul className="about__list">
+          <li className="about__card">
+            <img className="main__img" src={heroImage} alt="" />
+            <h4>Sign Up and Set Your Goals</h4>
+            <p>
+              Create an account and tell us about your fitness goals and current
+              fitness level.
+            </p>
+          </li>
+          <li className="about__card">
+            <img className="main__img" src={heroImage} alt="" />
+            <h4>Follow Your Personalized Workout Plan</h4>
+            <p>
+              Get access to customized workout programs designed specifically
+              for you. Follow along with real-time feedback to ensure proper
+              form and maximize results.
+            </p>
+          </li>
+          <li className="about__card">
+            <img className="main__img" src={heroImage} alt="" />
+            <h4>Track Your Progress and Stay Motivated</h4>
+            <p>
+              Monitor your progress with detailed analytics and reports.
+              Celebrate your achievements and stay motivated with our built-in
+              reward system.
+            </p>
+          </li>
+        </ul>
+        <button>Start Your Free Trial</button>
+      </section>
+
+      <h1 className="bold">
+        H1 The quick brown fox jumps over the lazy dog Bold{" "}
+      </h1>
+      <h2 className="semi-bold">
+        H2 The quick brown fox jumps over the lazy dog SemiBold
+      </h2>
+      <h3 className="medium">
+        H3 The quick brown fox jumps over the lazy dog Medium
+      </h3>
+      <h4 className="medium">
+        H4 The quick brown fox jumps over the lazy dog medium
+      </h4>
+      <h5 className="medium">
+        H5 The quick brown fox jumps over the lazy dog medium
+      </h5>
+
+      <p className="regular">
+        p The quick brown fox jumps over the lazy dog Regular
+      </p>
+      <p className="extra-light">
+        p The quick brown fox jumps over the lazy dog Extra Light
+      </p>
+      <button>Button The quick brown fox jumps over the lazy dog</button>
+    </>
+  );
+};
+
+export default HomePage;
