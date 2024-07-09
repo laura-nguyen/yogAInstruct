28d50121584d461cb444d9a0942fbf9d4a6c632a
Laura Nguyen <hello@lauranguyen.me>
2024-07-08 21:44:28 -0400

    added template for poses page

--- src/pages/PosesPage/PosesPage.jsx
+++ src/pages/PosesPage/PosesPage.jsx
@@ -1,5 +1,37 @@
+import { NavLink } from "react-router-dom";
+
 const PosesPage = () => {
-  return <div>PosesPage</div>;
+  return (
+    <main className="instructions">
+      <h1 className="instructions__header">Learn Yoga Poses</h1>
+      <div className="instructions__list">
+        <ul className="about__list">
+          <li className="about__card">
+            <div className="main__img">1️⃣</div>
+
+            <p>Accept camera access and wait for the webcam to load.</p>
+          </li>
+          <li className="about__card">
+            <div className="main__img">2️⃣</div>
+
+            <p>Perform the pose and hold for 15 seconds.</p>
+          </li>
+          <li className="about__card">
+            <div className="main__img">3️⃣</div>
+
+            <p>
+              The timer will restart if you make a mistake. Remember to breathe!
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div className="instructions__proceed">
+        <NavLink to="/stretches">
+          <button>I'm ready!</button>
+        </NavLink>
+      </div>
+    </main>
+  );
 };
 
 export default PosesPage;
