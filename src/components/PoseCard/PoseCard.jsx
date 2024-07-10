21a33f64f14d847061d6f1804420a7f5ad144209
Laura Nguyen <hello@lauranguyen.me>
2024-07-09 22:20:30 -0400

    styled yoga poses page

--- src/components/PoseCard/PoseCard.jsx
+++ src/components/PoseCard/PoseCard.jsx
@@ -9,16 +9,17 @@ const PoseCard = ({ pose }) => {
         <img className="card__img" src={image} alt="Lago di Braies" />
 
         <div className="card__details">
-          <div className="card__tags">
-            {pose.categories.map((category, index) => (
-              <span key={index} className="tag">
-                {category}
-              </span>
-            ))}
+          <div className="card__details-copy">
+            <div className="card__tags">
+              {pose.categories.map((category, index) => (
+                <span key={index} className="tag">
+                  {category}
+                </span>
+              ))}
+            </div>
+            <h4 className="name">{english_name}</h4>
+            <p>{pose_description}</p>
           </div>
-          <h4 className="name">{english_name}</h4>
-          <span>{pose.sanskrit_name_adapted}</span>
-          <p>{pose_description}</p>
           <NavLink to={`/practice/${pose.id}`}>
             <button className="card__button">Practice</button>
           </NavLink>
--- src/pages/PosesPage/PosesPage.jsx
+++ src/pages/PosesPage/PosesPage.jsx
@@ -1,10 +1,11 @@
 import poses from "../../data/yoga_poses.json";
 import PoseCard from "../../components/PoseCard/PoseCard";
+import "./PosesPage.scss";
 
 export const PosesPage = () => {
   return (
-    <main className="instructions">
-      <h1 className="instructions__header">Learn Yoga Poses</h1>
+    <main className="poses">
+      <h1 className="poses__header">Learn Yoga Poses</h1>
       <div className="poses__list">
         {poses.map((pose) => (
           <PoseCard key={pose.id} id={pose.id} pose={pose} />
